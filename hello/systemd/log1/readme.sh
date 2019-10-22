41adee3f96ad762c6a4da1cd3524f4d43aa2aa88
oudream <oudream@126.com>
2019-10-22 17:12:20 +0800

    Update readme.sh

--- hello/systemd/log1/readme.sh
+++ hello/systemd/log1/readme.sh
@@ -1,1 +1,7 @@
 #!/usr/bin/env bash
+
+ln -s /opt/ddd/cshell/hello/systemd/log1/timer1.service /etc/systemd/system/
+systemctl list-units --type=service
+systemctl enable timer1
+systemctl start timer1
+
