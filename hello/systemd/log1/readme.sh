290972bb52d55768681b545724cf71f53aedb486
oudream <oudream@126.com>
2019-10-23 13:54:07 +0800

    Update readme.sh

--- hello/systemd/log1/readme.sh
+++ hello/systemd/log1/readme.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-ln -s /opt/ddd/cshell/hello/systemd/log1/timer1.service /etc/systemd/system/
-
 systemctl list-units --type=service
 
+ln -s /opt/ddd/cshell/hello/systemd/log1/timer1.service /etc/systemd/system/
 systemctl enable timer1
 systemctl start timer1
 
@@ -11,4 +10,4 @@ sudo journalctl -f -u timer1
 
 systemctl stop timer1
 systemctl disable timer1
-
+rm /etc/systemd/system/timer1.service
