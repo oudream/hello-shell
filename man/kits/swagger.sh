2ab859d99d40f10b8c4e6ff725124e1e39c7a1f1
oudream <oudream@126.com>
2020-02-25 08:48:52 +0800

    Create swagger.sh

---
+++ man/kits/swagger.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+### doc
+open https://swagger.io/docs/open-source-tools/swagger-codegen/
+open https://swagger.io/docs/open-source-tools/swagger-ui/usage/installation/
+open https://swagger.io/docs/open-source-tools/swagger-editor/
+open https://swagger.io/
+
+
+###
+open https://swagger.io/tools/swagger-ui/
+
+### source code
+open https://github.com/swagger-api
+open https://github.com/swagger-api/swagger-codegen
+open https://github.com/swagger-api/swagger-ui
+open https://github.com/swagger-api/swagger-editor
+open https://github.com/swagger-api/swagger-core
+open https://github.com/swagger-api/swagger-samples
+open https://github.com/swagger-api/swagger-node
+
