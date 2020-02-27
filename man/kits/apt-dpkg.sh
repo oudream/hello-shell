6512ed7cf09396f3cdcc68aa34792e112914bd89
oudream <oudream@126.com>
2020-02-27 10:14:23 +0800

    Update apt-dpkg.sh

--- man/kits/apt-dpkg.sh
+++ man/kits/apt-dpkg.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+wget -qO - https://deb.opera.com/archive.key | sudo apt-key add -
+wget -qO - https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+
+   - name: add Kubernetes apt-key
+     apt_key:
+       url: https://packages.cloud.google.com/apt/doc/apt-key.gpg
+       state: present
+
+   - name: add Kubernetes' APT repository
+     apt_repository:
+      repo: deb http://apt.kubernetes.io/ kubernetes-xenial main
+      state: present
+      filename: 'kubernetes'
+
+
 ### 程序
 rpm -qa # 查看所有安装的软件包
 ## dpkg
@@ -37,19 +53,29 @@ dpkg -s package #报告指定包的状态信息
 dpkg -l #显示所有已经安装的Deb包，同时显示版本号以及简短说明
 dpkg -L python3-dev
 
-## apt-get , apt
-# apt-get是一条linux命令，适用于deb包管理式的操作系统，主要用于自动从互联网的软件仓库中搜索、安装、升级、卸载软件或操作系统。
-apt-get update # 升级安装包相关的命令,刷新可安装的软件列表(但是不做任何实际的安装动作)
-apt-get upgrade # 进行安装包的更新(软件版本的升级)
-apt-get dist-upgrade # 进行系统版本的升级(Ubuntu版本的升级)；如果系统提示某些软件包会被“保留”而不能被升级，则可以用 apt-get dist-upgrade 命令来升级所有软件包：
-do-release-upgrade # Ubuntu官方推荐的系统升级方式,若加参数-d还可以升级到开发版本,但会不稳定
-apt-get install [软件名称] # 安装一个新软件包
-apt-get remove [软件名称] # 卸载一个已安装的软件包（保留配置文档）
-apt-get remove --purge [软件名称] # 卸载一个已安装的软件包（删除配置文档）
-apt-get autoremove [软件名称] # 删除包及其依赖的软件包
-apt-get autoremove --purge [软件名称] # 删除包及其依赖的软件包+配置文件，比上面的要删除的彻底一点
-dpkg --force-all --purge [软件名称] # 有些软件很难卸载，而且还阻止了别的软件的应用，就能够用这个，但是有点冒险。
+## apt, apt-get
+# apt, apt-get是一条linux命令，适用于deb包管理式的操作系统，主要用于自动从互联网的软件仓库中搜索、安装、升级、卸载软件或操作系统。
+apt update # 升级安装包相关的命令,刷新可安装的软件列表(但是不做任何实际的安装动作)
+apt upgrade # 进行安装包的更新(软件版本的升级)
+apt dist-upgrade # 进行系统版本的升级(Ubuntu版本的升级)；如果系统提示某些软件包会被“保留”而不能被升级，则可以用 apt dist-upgrade 命令来升级所有软件包：
+apt install [软件名称] # 安装一个新软件包
+apt remove [软件名称] # 卸载一个已安装的软件包（保留配置文档）
+apt remove --purge [软件名称] # 卸载一个已安装的软件包（删除配置文档）
+apt autoremove [软件名称] # 删除包及其依赖的软件包
+apt autoremove --purge [软件名称] # 删除包及其依赖的软件包+配置文件，比上面的要删除的彻底一点
 apt list --installed # 列出所有已经安装的包
+dpkg --force-all --purge [软件名称] # 有些软件很难卸载，而且还阻止了别的软件的应用，就能够用这个，但是有点冒险。
+do-release-upgrade # Ubuntu官方推荐的系统升级方式,若加参数-d还可以升级到开发版本,但会不稳定
+apt list #  list packages based on package names
+apt search # search in package descriptions
+apt show # show package details
+apt install # install packages
+apt remove # remove packages
+apt autoremove # Remove automatically all unused packages
+apt update # update list of available packages
+apt upgrade # upgrade the system by installing/upgrading packages
+apt full#upgrade # upgrade the system by removing/installing/upgrading packages
+apt edit#sources # edit the source information file
 
 
 ## Snap
@@ -74,6 +100,7 @@ sudo snap remove xxxx
 # 有什么snap软件包呢！
 # Uappexplorer
 # https://uappexplorer.com/snaps
+
 
 openjdk-11-jre-headless/now 11.0.4+11-1ubuntu2~18.04.3 amd64 [installed,upgradable to: 11.0.5+10-0ubuntu1.1~18.04]
 openjdk-8-jdk-headless/now 8u222-b10-1ubuntu1~18.04.1 amd64 [installed,upgradable to: 8u232-b09-0ubuntu1~18.04.1]
